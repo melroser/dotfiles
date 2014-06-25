db98f27d35cca95b953e8e70eff84abc2f46530d
Robert R Melrose <robert.r.melrose@seagate.com>
2014-06-25 11:35:52 -0600

    cleaned up dots

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ export MOBILE_DOTFILES=`pwd`/homedir/
 ln -s $MOBILE_DOTFILES/bashrc.sh $HOME/.bashrc
 ln -s $MOBILE_DOTFILES/bash_profile.sh $HOME/.bash_profile
 ln -s $MOBILE_DOTFILES/bash_history.sh $HOME/.bash_history
-
 ln -s $MOBILE_DOTFILES/aliases.sh $HOME/.aliases
 ln -s $MOBILE_DOTFILES/inputrc.sh $HOME/.inputrc
+ln -s $MOBILE_DOTFILES/prompt.sh $HOME/.prompt
+ln -s $MOBILE_DOTFILES/exports.sh $HOME/.exports
