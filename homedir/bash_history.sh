a06b78188ae4e4dbdee0fa6cb5fa1f2ae234fc8e
melroser <themelroser@gmail.com>
2014-06-25 21:28:53 -0600

    changes to history

--- homedir/bash_history.sh
+++ homedir/bash_history.sh
@@ -2,3 +2,238 @@ ls
 la
 cd
 la
+la
+..
+vim /Users/robert/.aliases 
+cd
+cd Workspace/projects/mobile_dotfiles/
+ls
+git status 
+git add -A
+git commit -m "first draft"
+git push origin master 
+git status 
+ssh root@192.168.1.100
+ssh root@192.168.1.100
+exit
+..
+ls
+la
+ll
+source .aliases 
+cd
+ls
+ls -a
+source .aliases 
+rm -rf .aliases 
+source .bashrc 
+source .bash_profile 
+cd Workspace/projects/mobile_dotfiles/o
+cd Workspace/projects/mobile_dotfiles/
+ls
+vim setup.sh 
+rm -rf ~/.aliases
+rm -rf ~/.inputrc
+source setup.sh 
+la
+ls
+..
+vim /Users/robert/.aliases 
+ls
+pwd
+git status 
+mv homedir/bin/* homedir/
+mv -r homedir/bin/* homedir/
+mv -rf homedir/bin/* homedir/
+mv -f homedir/bin/* homedir/
+cd homedir/bin/
+ls
+ls -a
+mv -rf vim ../vim/
+cd ..
+cd vim/
+ls -a
+cd ..
+ls
+cd bin/
+ls -a
+rm -rf vim 
+cd ..
+ls
+ls -a
+rm -rf bin
+ls
+cd ..
+ls
+vim setup.sh 
+ls
+la
+..
+vim 
+ls
+ls
+la
+cd Workspace/projects/mobile_dotfiles/
+ls
+source setup.sh 
+vim setup.sh 
+source setup.sh 
+cd /Users/robert/bin/
+ls
+ls -a
+source aliases 
+source inputrc 
+..
+cd ..
+ls
+vim .bash_profile 
+ls
+vim Workspace/projects/mobile_dotfiles/setup.sh 
+cd Workspace/projects/
+ls
+cd mobile_dotfiles/
+ls
+source setup.sh 
+mv ~/.inputrc ~/Workspace/temp/
+source setup.sh 
+cd
+ls
+ls
+la
+..
+source ~/.aliases 
+source ~/.aliases 
+cd ~
+rm -rf .aliases 
+rm -rf .inputrc 
+cd ..
+cd robert/
+ls
+ls -a
+rm -rf .backup_dotfiles
+cd Workspace/projects/mobile_dotfiles/
+ls 
+source setup.sh 
+ls
+la
+source ~/bin/aliases 
+source ~/bin/inputrc 
+vim /Users/robert/.bash_profile 
+ls
+cd homedir/
+ls
+mkdir vim
+ls
+cd vim/
+ls
+pwd
+ll
+ls
+ls -a
+..
+cd ..
+ls -a
+mv .vim/* vim/
+rm -rf .vim/
+ls
+ls -a
+vim bin/aliases.sh 
+rm -rf ~/bin/aliases 
+rm -rf ~/bin/inputrc 
+cd
+ls
+ls -a
+mv .backup_dotfiles Workspace/temp/
+ls
+ls
+vim .bash_profile 
+ls
+cd Workspace/projects/mobile_dotfiles/
+ls
+cd homedir/
+ls
+mkdir bin
+ls
+cd bin/
+ls
+touch aliases.sh
+touch exports.sh
+touch vim exports.sh 
+vim exports.sh 
+vim aliases.sh 
+touch inputrc.sh
+vim inputrc.sh 
+touch functions.sh
+vim functions.sh 
+touch prompt.sh
+vim prompt.sh 
+ls
+..
+ls
+cd ..
+ls
+..
+ls
+cd ..
+ls
+vim setup.sh 
+vim homedir/bin/osx.sh
+ls
+vim setup.sh 
+source setup.sh 
+vim setup.sh 
+ls
+source setup.sh 
+rm -rf ~/bin/aliases 
+rm -rf ~/bin/inputrc 
+source setup.sh 
+ls
+la
+ls -a
+ln -s Workspace/projects/mobile_dotfiles/homedir/bashrc.sh ~/.bashrc
+ln -s ${MOBILE_DOTFILES}/homedir/bash_profile.sh ~/.bash_profile
+ls
+la
+ls -a
+cat .bash_profile 
+rm .bash_profile 
+export MOBILE_DOTFILES=Workspace/projects/mobile_dotfiles/homedir
+
+cat .bash_profile 
+export MOBILE_DOTFILES=/Users/robert/Workspace/projects/mobile_dotfiles/homedir
+ln -s ${MOBILE_DOTFILES}/homedir/bash_profile.sh /Users/robert/.bash_profile
+rm .bash_profile 
+ln -s ${MOBILE_DOTFILES}/homedir/bash_profile.sh /Users/robert/.bash_profile
+rm .bashrc 
+ln -s ${MOBILE_DOTFILES}/homedir/bashrc.sh /Users/robert/.bashrc
+cat .bashrc 
+ls
+ls -a
+ln -s /Users/robert/Workspace/projects/mobile_dotfiles/homedir/bashrc.sh /Users/robert/.bashrc
+rm -rf .bashrc 
+ln -s /Users/robert/Workspace/projects/mobile_dotfiles/homedir/bashrc.sh /Users/robert/.bashrc
+cat .bashrc 
+cat .bash_profile 
+rm -rf .bash_profile 
+ln -s /Users/robert/Workspace/projects/mobile_dotfiles/homedir/bash_profile.sh /Users/robert/.bash_profile
+cat .bash_profile 
+ls
+ls -a
+vim .bashrc 
+ls
+cd Workspace/projects/mobile_dotfiles/
+ls
+touch setup.sh
+vim setup.sh 
+ls
+vim setup.sh 
+sh setup.sh 
+source setup.sh 
+echo $MOBILE_DOTFILES 
+vim setup.sh 
+ls
+rm ~/.bash*
+ls
+source setup.sh 
+ls -a ~
+cd
